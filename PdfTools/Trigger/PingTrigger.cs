a8c3525c1065df2b801d78ec5ec520e18716c492
Neto Batista <netosantosbatista@outlook.com>
2024-11-02 14:40:36 -0300

    feat: add ping trigger

---
+++ PdfTools/Trigger/PingTrigger.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace PdfTools.Trigger;
+
+public class PingTrigger
+{
+    [Function("Ping")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+    {
+       return new NoContentResult();
+    }
+}
---
+++ PdfToolsTest/Trigger/PingTriggerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PdfTools.Trigger;
+using PdfTools.Dto;
+using PdfTools.Interface;
+using PdfTools.Model;
+
+namespace PdfToolsTest.Trigger;
+
+[TestClass]
+public class PingTriggerTest
+{
+    [TestMethod("Should be ping")]
+    public async Task ConvertHtmlToPdfSuccess()
+    {
+        var request =new Mock<HttpRequest>();
+        var route = new PingTrigger();
+        var response = route.Run(request.Object);
+        Assert.AreSame(typeof (NoContentResult), response.GetType());
+    }
+    
+}
